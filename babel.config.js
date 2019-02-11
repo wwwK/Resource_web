0c38b5b269ac0bf9d0a527f056f684f40fff4480
邓占勇 <2657179843@qq.com>
2019-02-11 16:10:07 +0800

    测试

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    ['@vue/app', {
+      polyfills: [ //Polyfill 是一块代码（通常是 Web 上的 JavaScript），用来为旧浏览器提供它没有原生支持的较新的功能。
+        'es6.promise',
+        'es6.symbol'
+      ]
+    }]
   ]
 }
